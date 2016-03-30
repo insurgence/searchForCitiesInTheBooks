47259bf8856d71e06c40f6439a28e75b778bea18
vadim <insurgence@mail.ru>
2016-03-31 02:25:40 +0400

    Added Fuzzy lib with HammigDistance, reading line with City:Country to write in path

--- searchForCitiesInTheBooks/Program.cs
+++ searchForCitiesInTheBooks/Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using eBdb.EpubReader;
+using FuzzyString;
 
 namespace searchForCitiesInTheBooks
 {
@@ -36,13 +37,77 @@ namespace searchForCitiesInTheBooks
             string[] res = sb.ToString().Split(new Char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
             sb.Clear();
 
-            List<string> ls = new List<string>();
+            List<string> lines = new List<string>();
 
             foreach (string s in res)
             {
                 if (char.IsUpper(s[0]))
-                    ls.Add(s);
+                    lines.Add(s);
             }
+
+            List<FuzzyStringComparisonOptions> options = new List<FuzzyStringComparisonOptions>();
+
+            options.Add(FuzzyStringComparisonOptions.UseHammingDistance);
+            options.Add(FuzzyStringComparisonOptions.UseLongestCommonSubsequence);
+            options.Add(FuzzyStringComparisonOptions.UseLongestCommonSubstring);
+
+            FuzzyStringComparisonTolerance tolerance = FuzzyStringComparisonTolerance.Strong;
+
+            string[] file = File.ReadAllLines(args[1]);
+            string path = args[2];
+
+            List<string> cities = new List<string>();
+            List<string> countries = new List<string>();
+
+            foreach (string src in lines)
+            {
+                foreach (string temp in file)
+                {
+                    string[] arr = temp.Split(':');
+
+                    string cityInTheFile = arr[0];
+                    string countryInTheFile = arr[1];
+
+                    bool boolCity = src.ApproximatelyEquals(cityInTheFile, options, tolerance);
+                    bool boolCountry = src.ApproximatelyEquals(countryInTheFile, options, tolerance);
+
+                    if (boolCity)
+                        cities.Add(cityInTheFile);
+
+                    if (boolCountry)
+                        countries.Add(countryInTheFile);
+                }
+
+                if (cities.Count() > 0)
+                {
+                    String.Join(", ", cities);
+                    cities.Insert(0, src + " : ");
+                }
+
+                if (countries.Count() > 0)
+                {
+                    String.Join(", ", countries);
+                    countries.Insert(0, src + " : ");
+                }
+
+                File.WriteAllLines(path, cities);
+                File.WriteAllLines(path, cities);
+            }
+
+
+
+
+
+
+            string source = "Китай";
+            string target = "Китае";
+
+            bool result = source.ApproximatelyEquals(target, options, tolerance);
+            
+            Console.WriteLine(result);
+
+            //string[] cities = File.ReadAllLines(args[1]);
+            //string[] countries = File.ReadAllLines(args[2]);
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
